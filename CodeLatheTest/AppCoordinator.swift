f7bf2ec41cb86ce262ba8278fdc8fd1404c85bfc
Ravi Sankar <4372447+ravisankar-r@users.noreply.github.com>
2021-01-18 14:09:05 +0530

    Use Coordinator pattern to lauch app's initial screen
    Use Coordinator pattern to lauch app's initial screen

---
+++ CodeLatheTest/AppCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  AppCoordinator.swift
+//  CodeLatheTest
+//
+//  Created by Ravisankar on 18/01/21.
+//
+
+import UIKit
+
+class AppCoordinator {
+    
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        
+    }
+}
