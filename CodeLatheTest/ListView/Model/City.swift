d439d4fe51ee115237f8844cfb04f9932decf369
Ravi Sankar <4372447+ravisankar-r@users.noreply.github.com>
2021-01-18 14:27:44 +0530

    Added Model to represent city

---
+++ CodeLatheTest/ListView/Model/City.swift
@@ -0,0 +1,15 @@
+//
+//  City.swift
+//  CodeLatheTest
+//
+//  Created by Ravisankar on 18/01/21.
+//
+
+import Foundation
+
+struct City {
+    
+    let name: String
+    let country: String
+    let state: String
+}
